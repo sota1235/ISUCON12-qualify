f270538eb05091ebe83e20b9ff34e98085020720
Ryusuke Chiba <chiba.r221@gmail.com>
2022-07-23 16:17:42 +0900

    Fix init script

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -14,6 +14,11 @@ mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < additional_schema.sql
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql
 
 # MySQL を dump から restore
